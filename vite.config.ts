b029d99cc2147d241ec55249ca0944e16ef9d384
sanelisekile99 <nqabenisanelisekile465@gmail.com>
2026-03-23 12:28:00 +0200

    trying to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.GITHUB_ACTIONS === "true"
+    ? `/${process.env.GITHUB_REPOSITORY?.split("/")[1] || ""}/`
+    : "/",
   server: {
     host: "::",
     port: 8080,
